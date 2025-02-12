a462e81190ed447b8e0c2210ba5d9d5bff1d3993
Akelvino <vinho892@gmail.com>
2025-02-12 21:39:04 +0300

    crated the listview detail view

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import TaskList, DetailView,TaskCreate
 
 urlpatterns = [
-    path("", home, name='home')
+    path("", TaskList.as_view(), name='home'),
+    path('task/<int:pk>/',DetailView.as_view(), name='task'),
+    path('task-create/',TaskCreate.as_view(), name='task-create'),
 ]
