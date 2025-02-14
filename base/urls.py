d3a7e3ed17867a4533826f8feb75ccd3c8064735
Akelvino <vinho892@gmail.com>
2025-02-14 17:43:42 +0300

    added login feature

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import TaskList, DetailView,TaskCreate, TaskUpdate,DeleteView
+from .views import TaskList, DetailView,TaskCreate, TaskUpdate,DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", TaskList.as_view(), name='home'),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('task-create/',TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/',TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/',DeleteView.as_view(), name='task-delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page ='login'), name='logout'),
 ]
